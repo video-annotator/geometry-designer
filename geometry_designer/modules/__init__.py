9f187215910bce606afe52cb9caa29b920285b75
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-03-23 18:52:23 +0000

    .

---
+++ geometry_designer/modules/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Ricardo Ribeiro"
+__copyright__ = "Copyright 2016 Champalimaud Foundation"
+__credits__ = "Ricardo Ribeiro"
+__license__ = "MIT"
+__version__ = "0.0.1.dev1"
+__maintainer__ = ["Ricardo Ribeiro", "Carlos Mão de Ferro"]
+__email__ = ["ricardojvr at gmail.com", "cajomferro at gmail.com"]
+__status__ = "Development"
