8d1c8164bd7b9656bca78caa75158cd16a297c54
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-07-12 13:53:46 +0100

    Update versioning system

--- setup.py
+++ setup.py
@@ -6,17 +6,25 @@ __author__ = "Ricardo Ribeiro"
 __copyright__ = ""
 __credits__ = "Ricardo Ribeiro"
 __license__ = "MIT"
-__version__ = "0.3"
 __maintainer__ = ["Ricardo Ribeiro", "Carlos Mão de Ferro"]
 __email__ = ["ricardojvr at gmail.com", "cajomferro at gmail.com"]
 __status__ = "Development"
 __updated__ = "2016-03-14"
 
 from setuptools import setup, find_packages
+import re, os
+
+PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(PACKAGE_PATH, 'geometry_designer','__init__.py'), 'r') as fd:
+    content = fd.read()
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1)
+
+
 
 setup(
     name='geometry_designer',
-    version=__version__,
+    version=version,
     description="""""",
     author='Ricardo Ribeiro',
     author_email='ricardo.ribeiro@research.fchampalimaud.org',
